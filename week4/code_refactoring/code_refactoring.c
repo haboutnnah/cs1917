88139d594ee8052545210221155db95bf360d4f4
Hannah Ivy <contact@hannahi.com>
2019-03-06 18:11:37 +1100

    asjdhajd

--- week4/code_refactoring/code_refactoring.c
+++ week4/code_refactoring/code_refactoring.c
@@ -1,96 +1,157 @@
-    /*
-     * Test if a point is inside a triangle.
-     * Julian Saknussemm
-     *
-     * Given Three points of a triangle, and another arbitrary point this program determines if that point lies inside
-     * the triangle.
-     *
-     * This is determined by satisfying the following rule:
-     * A point P(x,y) is inside triangle A(x0,y0), B(x1,y1), C(x2,y2)
-     * iff
-     * P is on the same side of the line AB as C
-     * P is on the same side of the line BC as A
-     * and
-     * P is on the same side of the line AC as B
-     *
-     * A special case exits for a vertical line (inf gradient) when testing the side of the line
-     */
+/*
+* Test if a point is inside a triangle.
+* Julian Saknussemm
+*
+* Given Three points of a triangle, and another arbitrary point this program determines if that point lies inside
+* the triangle.
+*
+* This is determined by satisfying the following rule:
+* A point P(x,y) is inside triangle A(x0,y0), B(x1,y1), C(x2,y2)
+* iff
+* P is on the same side of the line AB as C
+* P is on the same side of the line BC as A
+* and
+* P is on the same side of the line AC as B
+*
+* A special case exits for a vertical line (inf gradient) when testing the side of the line
+*/
      
-    #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define IS_ABOVE_LINE 1
+#define IS_ON_LINE    0
+#define IS_UNDER_LINE -1
+
+#define TRUE  1
+#define FALSE 0
+
+int isAboveLine( double px, double py, double m, double b );
+int testThreePoints (double x0,double y0,
+                     double x1,double y1,
+                     double x2,double y2,
+                     double px, double py);
+int pointsOnSameSide (double px, double py, 
+                      double m,double b, 
+                      double lx, double ly);
+void testProgram(void);
+
+int main (int argc, char* argv[]) {
+    testProgram();
+    double x0,y0,x1,y1,x2,y2;
+    double px,py;
      
-    int test2( double px, double py, double m, double b )
-    {   if( py < m * px + b ) {
-            return -1; // point is under line
-               }else if ( py == m * px + b ){
-               return 0;
-               } else {
-               return 1; // over
-            }
-    }
-     
-    int // two points lie on the same side of a line
-    test1( double px, double py, double m,double b, double lx,double ly) {
-            return (test2( px,py, m,b )==test2(lx,ly,m,b));
-    }
-    int tritest(double x0,double y0,double x1,double y1,double x2,double y2,double px, double py){
-     
-            int line1, line2, line3;
-    // line eqns
-     
-    double m01 = (y1-y0)/(x1-x0);
-    // b: y - y1 = m( x - x1 ), x = 0
-    double b01 = m01 * -x1 + y1;
-    double m02, m12, b02, b12;
-    m02 = (y2-y0)/(x2-x0);
-    m12 = (y2-y1)/(x2-x1);
-    b02 = m02 * -x2 + y2;
-    b12 = m12 * -x2 + y2;
-     
-    // vertical line checks
-    if( x1 == x0 ) {
-       line1 = ( (px <= x0) == (x2 <= x0) );
-    } else {
-       line1 = test1( px, py, m01, b01,x2,y2);
-    }
-     
-    if( x1 == x2 ) {
-            line2 = ( (px <= x2) == (x0 <= x2) );
-    } else {
-            line2 = test1(px,py, m12, b12,x0,y0);
-    }
-     
-    if( x2 == x0 ) {
-    line3 = ( (px <= x0 ) == (x1 <= x0) );} else {
-            line3 = test1(px, py, m02,b02,x1,y1);
-    }
-     
-       return line1 && line2 && line3;
-     
-    }
-    int main(int argc, char* argv[]) {
-    double x0,y0,x1,y1,x2,y2,px;
-    double py;
-     
-    int scanfsReturnValueAggregatedOverAllScanfs = 0;
-     
+    int countValues = 0;
+    int exitStatus;
+
     // get input
-            printf("Triangle Vertex A (x,y): "); scanfsReturnValueAggregatedOverAllScanfs += scanf("%lf,%lf", &x0,&y0);
-            printf("Triangle Vertex  B (x,y): "); scanfsReturnValueAggregatedOverAllScanfs += scanf("%lf,%lf", &x1,&y1);
-            printf("Triangle Vertex  C (x,y): "); scanfsReturnValueAggregatedOverAllScanfs += scanf("%lf,%lf", &x2,&y2);
-       printf("Test Point (x,y): "); scanfsReturnValueAggregatedOverAllScanfs += scanf("%lf,%lf", &px,&py);
+    printf("Triangle Vertex A (x,y): ");
+    countValues += scanf("%lf,%lf", &x0,&y0);
+    printf("Triangle Vertex  B (x,y): ");
+    countValues += scanf("%lf,%lf", &x1,&y1);
+    printf("Triangle Vertex  C (x,y): ");
+    countValues += scanf("%lf,%lf", &x2,&y2);
+    printf("Test Point (x,y): ");
+    countValues += scanf("%lf,%lf", &px,&py);
      
     // print error
-       if( scanfsReturnValueAggregatedOverAllScanfs != 8 ) {
-                    printf("You're stupid and didn't put in the right inputs!\n");
-                    return 1;
+    if(countValues != 8 ) {
+        printf("Please only input what is asked, ");
+        printf("two values seperated by a comma.\n");
+        exitStatus = EXIT_FAILURE;
+    } else {
+        // print answer
+        printf("Point (%.2lf,%.2lf) is ", px,py);
+        
+        if(testThreePoints(x0, y0, x1, y1, x2, y2, px, py)) {
+            printf("inside");
+        } else {
+            printf("outside");
+        }
+        printf(" the triangle\n");
+        exitStatus = EXIT_SUCCESS;
     }
-     
-    // print answer
-       printf("Point (%.2lf,%.2lf) is ", px,py);
-       if(tritest(x0,y0,x1,y1,x2,y2,px,py)) printf("inside");
-       else printf("outside");
-       printf(" the Triangel\n");
-     
     // return 0
-    return 0;
+    return exitStatus;
+}
+
+void testProgram (void) {
+    printf("running tests . . . ");
+    assert(testThreePoints(3, 2, 2, 3, 0, 2, 2, 2) == FALSE);
+    assert(testThreePoints(1, 1, 1, 1, 1, 1, 1, 1) == TRUE);
+    assert(testThreePoints(11, 2, 7, 4, 4, 9, 10, 5) == FALSE);
+    assert(testThreePoints(11, 2, 7, 4, 4, 9, 15, 5) == FALSE);
+    assert(testThreePoints(11, 2, 7, 4, 4, 9, 15, 1) == FALSE);
+    printf("all tests passed");
+}
+
+int isAboveLine( double px, double py, double m, double b ) {
+    int lineStatus = IS_ABOVE_LINE;
+
+    if( py < m * px + b ) {
+        lineStatus =  IS_UNDER_LINE; 
+    } else if ( py == m * px + b ) {
+        lineStatus = IS_ON_LINE;
+    } 
+
+    return lineStatus;
+}
+
+// two points lie on the same side of a line  
+int pointsOnSameSide (double px, double py, 
+                      double m,double b, 
+                      double lx, double ly) {
+    int status = FALSE;
+    if (isAboveLine(px, py, m, b ) == isAboveLine(lx, ly ,m, b)) {
+        status = TRUE;
     }
+    return status;
+}
+    
+int testThreePoints (double x0,double y0,
+                     double x1,double y1,
+                     double x2,double y2,
+                     double px, double py){
+
+    int line1, line2, line3;
+    double gradient1, gradient2;
+    double constant1, constant2;
+    double gradient1and2, constant1and2;
+    int status = FALSE;
+    // line eqns
+     
+    gradient1 = (y1-y0)/(x1-x0);
+    constant1 = gradient1 * -x1 + y1;
+    gradient2 = (y2-y0)/(x2-x0);
+    gradient1and2 = (y2-y1)/(x2-x1);
+    constant2 = gradient2 * -x2 + y2;
+    constant1and2 = gradient1and2 * -x2 + y2;
+     
+    // vertical line checks
+    if ( x1 == x0 ) {
+       line1 = ( (px <= x0) == (x2 <= x0) );
+    } else {
+       line1 = pointsOnSameSide(px, py, gradient1, constant1, x2, y2);
+    }
+     
+    if ( x1 == x2 ) {
+            line2 = ( (px <= x2) == (x0 <= x2) );
+    } else {
+        line2 = pointsOnSameSide(px, py, 
+                                 gradient1and2, constant1and2,
+                                 x0, y0);
+    }
+     
+    if ( x2 == x0 ) {
+        line3 = ( (px <= x0 ) == (x1 <= x0) );
+    } else {
+        line3 = pointsOnSameSide(px, py, gradient2,constant2,x1,y1);
+    }
+    if (line1 && line2 && line3) {
+        status = TRUE;
+    }
+     
+    return status;
+}
+
